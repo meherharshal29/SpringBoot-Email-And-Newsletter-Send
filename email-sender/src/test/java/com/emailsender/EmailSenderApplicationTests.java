df0d84eb138c8327d379b14fb10f8c187c41f787
Harshal Meher <134125835+Harshal-Meher@users.noreply.github.com>
2025-08-28 17:56:22 +0530

    send email and newsletter

---
+++ email-sender/src/test/java/com/emailsender/EmailSenderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emailsender;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmailSenderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
